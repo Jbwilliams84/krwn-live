deb1b601b79324f8f9abfc50cb1f6ae1f6fb3e81
Jbwilliams84 <jbwilliams484@gmail.com>
2019-10-31 15:42:40 -0500

    updated reducer for async functions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 
-import logger from "redux-logger";
+import logger from 'redux-logger';
 
-import rootReducer from "./root-reducer";
+import thunk from 'redux-thunk';
 
-const middlewares = [logger];
+import rootReducer from './root-reducer';
+
+const middlewares = [logger, thunk];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
